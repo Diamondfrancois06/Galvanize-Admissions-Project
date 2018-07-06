ca2f4fda6f7c8043faf63f1ed64aa53df9a81fd3
Unknown <america.ibo.francois@gmail.com>
2018-07-06 02:19:51 -0700

    Added Local Files to Galvanize GitHub Directory
    My HTML, CSS, and JavaScript files are added to GitHub, using GitKraken! All of my code pertaining to my Badge Creator are available here!

---
+++ ID Badge Creator Files/scrap.js
@@ -0,0 +1,11 @@
+function idCard(){
+    document.getElementById('postFullName').innerHTML= 
+    document.getElementById('firstName').value + document.getElementById('lastName').value;
+    document.getElementById('postAge').innerHTML=
+    document.getElementById('age').value
+    document.getElementById('postPhoneNumber').innerHTML=
+    document.getElementById('phoneNumber').value
+    document.getElementById('postAddress').innerHTML=
+    document.getElementById('address').value
+
+}
